e0ad204ccb1597dae9d421e363c0dde596cdb414
darienmiller88 <darienmiller88@yahoo.com>
2022-10-01 14:54:29 -0400

    backend initialized

--- go.mod
+++ go.mod
@@ -3,9 +3,17 @@ module Girls-in-tech-Hackathon
 // +heroku goVersion go1.17
 go 1.17
 
+require github.com/go-chi/cors v1.2.1
+
 require (
+	github.com/fsnotify/fsnotify v1.5.4 // indirect
 	github.com/go-chi/chi v1.5.4 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.4 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/unrolled/render v1.5.0 // indirect
+	golang.org/x/crypto v0.0.0-20220926161630-eccd6366d1be // indirect
+	golang.org/x/sys v0.0.0-20220615213510-4f61da869c0c // indirect
 	gorm.io/gorm v1.23.10 // indirect
 )
--- main.go
+++ main.go
@@ -2,8 +2,33 @@ package main
 
 import( 
 	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/unrolled/render"
 )
 
 func main(){
+	godotenv.Load()
+	router := chi.NewRouter()
+	r := render.New()
+
+	cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000", "https://girlsintech.netlify.app"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowCredentials: true,
+	})
+
+	router.Get("/", func(res http.ResponseWriter, req *http.Request){
+		r.JSON(res, http.StatusOK, map[string]string{
+			"message": "deployed on heroku",
+		})
+	})
+
+
 	fmt.Println("start of project!")
+	http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), router)
 }
